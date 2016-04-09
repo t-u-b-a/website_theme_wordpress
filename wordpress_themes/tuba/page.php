d7879a94eef7399e6146ef559faca81c2e1b34f7
azai-yahoo <azai@yahoo-inc.com>
2016-04-10 03:46:44 +0800

    Move hero module from cms

--- wordpress_themes/tuba/page.php
+++ wordpress_themes/tuba/page.php
@@ -4,6 +4,15 @@
     <body <?php body_class(); ?>>
         <div class="wrap">
             {{>header}}
+            <?php if (is_front_page()) : ?>
+            <div class="hero">
+                <div class="img"></div>
+                <div class="title-wrap">
+                    <h2 class="title">大標題</h2>
+                    <h3 class="subtitle">小標題</h3>
+                </div>
+            </div>
+            <?php endif; ?>
             <div class="body Cf">
                 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
                 <div id="post-<?php the_ID(); ?>" <?php post_class('page'); ?>>
